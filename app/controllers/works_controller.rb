396acf6baf4ca926d40b56ceab50d2a640e136dd
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-04-17 17:01:07 -0700

    HAHA, can logout now

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,7 +2,7 @@ class WorksController < ApplicationController
   # We should always be able to tell what category
   # of work we're dealing with
 
-  # before_action :require_login, except: [:index]
+  before_action :require_login, except: [:root]
   before_action :category_from_url, only: [:index, :new, :create]
   before_action :category_from_work, except: [:root, :index, :new, :create]
 
